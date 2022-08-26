67f91906618fa5ba2b27f9f3444d3ac287e5ca0a
seelevolet <476405271@qq.com>
2022-08-26 10:19:07 +0800

    登录

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="login-container">
-    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
-
+    <el-form
+      ref="loginForm"
+      :model="loginForm"
+      :rules="loginRules"
+      class="login-form"
+      auto-complete="on"
+      label-position="left"
+    >
       <div class="title-container">
-        <h3 class="title">Login Form</h3>
+        <h3 class="title">谷粒学院后台管理系统</h3>
       </div>
 
       <el-form-item prop="username">
@@ -20,7 +26,7 @@
           auto-complete="on"
         />
       </el-form-item>
-
+      <!-- 密码框部分 -->
       <el-form-item prop="password">
         <span class="svg-container">
           <svg-icon icon-class="password" />
@@ -37,100 +43,156 @@
           @keyup.enter.native="handleLogin"
         />
         <span class="show-pwd" @click="showPwd">
-          <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
+          <svg-icon
+            :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'"
+          />
         </span>
       </el-form-item>
+      <!-- 验证码部分 -->
+<!--      <div align="center" style="margin-bottom: 20px" v-if="enableVerify">-->
+<!--        <slide-verify-->
+<!--          :l="42"-->
+<!--          :r="10"-->
+<!--          :w="310"-->
+<!--          :h="155"-->
+<!--          @success="onSuccess"-->
+<!--          @fail="onFailure"-->
+<!--          @refresh="onRefresh"-->
+<!--          :slider-text="verifyText"-->
+<!--        ></slide-verify>-->
+<!--        <div align="center" style="margin-top: 10px">{{ verifyMessage }}</div>-->
+<!--      </div>-->
 
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
-
-      <div class="tips">
-        <span style="margin-right:20px;">username: admin</span>
-        <span> password: any</span>
-      </div>
+      <el-button
+        :loading="loading"
+        type="primary"
+        style="width: 100%; margin-bottom: 30px"
+        @click.native.prevent="handleLogin"
+        >登录</el-button
+      >
 
     </el-form>
   </div>
 </template>
 
 <script>
-import { validUsername } from '@/utils/validate'
+import { validUsername } from "@/utils/validate";
 
 export default {
-  name: 'Login',
+  name: "Login",
   data() {
-    const validateUsername = (rule, value, callback) => {
-      if (!validUsername(value)) {
-        callback(new Error('Please enter the correct user name'))
-      } else {
-        callback()
-      }
-    }
-    const validatePassword = (rule, value, callback) => {
-      if (value.length < 6) {
-        callback(new Error('The password can not be less than 6 digits'))
-      } else {
-        callback()
-      }
-    }
     return {
       loginForm: {
-        username: 'admin',
-        password: '111111'
+        username: "admin",
+        password: "111111",
       },
       loginRules: {
-        username: [{ required: true, trigger: 'blur', validator: validateUsername }],
-        password: [{ required: true, trigger: 'blur', validator: validatePassword }]
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          { min: 6, message: "密码最小为6位" },
+        ],
       },
       loading: false,
-      passwordType: 'password',
-      redirect: undefined
-    }
+      passwordType: "password",
+      redirect: undefined,
+      // 是否显示滑动验证框
+      enableVerify: false,
+      // 滑动验证框信息
+      verifyMessage: "",
+      // 滑动验证框内部文字提示
+      verifyText: "向右滑动滑块",
+      // 消息框
+      messageBox: {},
+    };
   },
   watch: {
     $route: {
-      handler: function(route) {
-        this.redirect = route.query && route.query.redirect
+      handler: function (route) {
+        this.redirect = route.query && route.query.redirect;
       },
-      immediate: true
-    }
+      immediate: true,
+    },
+  },
+  created() {
+    this.messageBox = this.$createElement;
   },
   methods: {
     showPwd() {
-      if (this.passwordType === 'password') {
-        this.passwordType = ''
+      if (this.passwordType === "password") {
+        this.passwordType = "";
       } else {
-        this.passwordType = 'password'
+        this.passwordType = "password";
       }
       this.$nextTick(() => {
-        this.$refs.password.focus()
-      })
+        this.$refs.password.focus();
+      });
     },
+    // 登录按钮点击后的事件
     handleLogin() {
-      this.$refs.loginForm.validate(valid => {
+      this.$refs.loginForm.validate((valid) => {
         if (valid) {
-          this.loading = true
-          this.$store.dispatch('user/login', this.loginForm).then(() => {
-            this.$router.push({ path: this.redirect || '/' })
-            this.loading = false
-          }).catch(() => {
-            this.loading = false
-          })
+          this.tryLogin();
+          // this.enableVerify = true;
+          // this.$notify({
+          //   title: "提示",
+          //   message: this.messageBox(
+          //     "i",
+          //     { style: "color: teal" },
+          //     "请滑动验证码"
+          //   ),
+          //   duration: 2000,
+          // });
         } else {
-          console.log('error submit!!')
-          return false
+          this.$message({
+            type: "error",
+            message: "账号或密码不符合验证规定",
+          });
+          return false;
         }
-      })
-    }
-  }
-}
+      });
+    },
+    // 发送登录请求
+    tryLogin() {
+      this.loading = true;
+      this.$store
+        .dispatch("user/login", this.loginForm)
+        .then(() => {
+          this.$router.push({ path: this.redirect || "/" });
+          this.loading = false;
+        })
+        .catch(() => {
+          this.loading = false;
+        });
+    },
+    // 验证码成功
+    onSuccess(times) {
+      this.verifyMessage = `验证成功, 耗时${(times / 1000).toFixed(1)}s`;
+      // 等待1s后再登录
+      setTimeout(() => {
+        this.tryLogin();
+      }, 1000);
+    },
+    // 验证码失败
+    onFailure() {
+      this.verifyMessage = "";
+    },
+    // 重新刷新验证码
+    onRefresh() {
+      this.verifyMessage = "";
+    },
+  },
+};
 </script>
 
 <style lang="scss">
 /* 修复input 背景不协调 和光标变色 */
 /* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
 
-$bg:#283443;
-$light_gray:#fff;
+$bg: #283443;
+$light_gray: #fff;
 $cursor: #fff;
 
 @supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
@@ -173,14 +235,26 @@ $cursor: #fff;
 </style>
 
 <style lang="scss" scoped>
-$bg:#2d3a4b;
-$dark_gray:#889aa4;
-$light_gray:#eee;
+$bg: #2d3a4b;
+$dark_gray: #135071;
+$light_gray: #eee;
 
 .login-container {
-  min-height: 100%;
+  position: fixed;
+  height: 100%;
   width: 100%;
-  background-color: $bg;
+  background-image: url("../../assets/images/background.jpg");
+  background-size: 100% 100%;
+  /* 背景图垂直、水平均居中 */
+  background-position: center center;
+  /* 背景图不平铺 */
+  background-repeat: no-repeat;
+  /* 当内容高度大于图片高度时，背景图像的位置相对于viewport固定 */
+  background-attachment: fixed;
+  /* 让背景图基于容器大小伸缩 */
+  background-size: cover;
+  /* 设置背景颜色，背景图加载过程中会显示背景色 */
+  background-color: #464646;
   overflow: hidden;
 
   .login-form {
@@ -192,17 +266,6 @@ $light_gray:#eee;
     overflow: hidden;
   }
 
-  .tips {
-    font-size: 14px;
-    color: #fff;
-    margin-bottom: 10px;
-
-    span {
-      &:first-of-type {
-        margin-right: 16px;
-      }
-    }
-  }
 
   .svg-container {
     padding: 6px 5px 6px 15px;
